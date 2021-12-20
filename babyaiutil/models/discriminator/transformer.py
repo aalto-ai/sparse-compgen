bb83188e264ebeba4c168bc6d138c66fd520b533
Sam Spilsbury <smspillaz@gmail.com>
2021-12-21 01:23:22 +0200

    transformer: Use new "Masked" model in harness, warmup lr

--- babyaiutil/models/discriminator/transformer.py
+++ babyaiutil/models/discriminator/transformer.py
@@ -203,10 +203,51 @@ class TransformerEncoderDecoderMasked(nn.Module):
         )
 
 
+def linear_with_warmup_schedule(
+    optimizer, num_warmup_steps, num_training_steps, min_lr_scale, last_epoch=-1
+):
+    min_lr_logscale = min_lr_scale
+
+    def lr_lambda(current_step):
+        # Scale from 0 to 1
+        if current_step <= num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+
+        # Scale from 1 to min_lr_scale logarithmically
+        #
+        # So for example, if min_lr_logscale is -3, then
+        # scale goes from 0 to -3 meaning that the lr multiplier
+        # goes from 1, to 1e-1 at -1, to 1e-2 at -2 to 1e-3 at -3.
+        scale = min(
+            1,
+            float(current_step - num_warmup_steps)
+            / float(num_training_steps - num_warmup_steps),
+        )
+        logscale = scale * min_lr_logscale
+        multiplier = 10 ** logscale
+
+        return multiplier
+
+    return torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda, last_epoch)
+
+
 class TransformerDiscriminatorHarness(ImageDiscriminatorHarness):
     def __init__(self, attrib_offsets, emb_dim, n_words, lr=10e-4):
         super().__init__(lr=lr)
-        self.encoder = TransformerEncoderDecoderModel(attrib_offsets, emb_dim, n_words)
+        self.encoder = TransformerEncoderDecoderMasked(attrib_offsets, emb_dim, n_words)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": linear_with_warmup_schedule(
+                    optimizer, 2000, self.trainer.max_steps, -2
+                ),
+                "interval": "step",
+                "frequency": 1,
+            },
+        }
 
     def forward(self, x):
         image, mission, direction = x
