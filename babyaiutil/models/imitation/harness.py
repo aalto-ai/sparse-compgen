58c346abf790a6a6c255850f03604cee8ae12516
Sam Spilsbury <smspillaz@gmail.com>
2022-03-17 18:47:58 +0200

    imitation/harness: Split out training harness functions from class

--- babyaiutil/models/imitation/harness.py
+++ babyaiutil/models/imitation/harness.py
@@ -30,60 +30,78 @@ def compute_conservative_policy_loss(policy_logits, taken_actions, returns):
     ).mean()
 
 
+def imitation_optimizer_config(model, lr):
+    optimizer = torch.optim.Adam(self.parameters(), self.hparams.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=1000, gamma=0.9
+    )
+
+    return {"optimizer": optimizer, "scheduler": scheduler}
+
+
+def imitation_training_step(harness, x, idx):
+    seeds, mission, images_path, directions_path, taken_actions, returns, masks = x
+    bool_masks = masks.bool()
+
+    past_actions = (taken_actions * (taken_actions != -1))[:, :-1]
+    policy_logits, critic_values = harness(
+        (mission, images_path, directions_path, past_actions)
+    )
+
+    loss = compute_imitation_loss(
+        policy_logits[bool_masks], taken_actions[bool_masks]
+    )
+    entropy = (
+        torch.special.entr(torch.softmax(policy_logits, dim=-1).clamp(10e-7, 1.0))
+        .sum(dim=-1)
+        .mean()
+    )
+
+    harness.log("tloss", loss, prog_bar=True)
+    harness.log("tentropy", entropy, prog_bar=True)
+    return loss - harness.hparams.entropy_bonus * entropy
+
+
+def imitation_episodic_success_validation_step(harness, x, idx, dl_idx):
+    rewards = x
+    success = (rewards > 0).to(torch.float).mean()
+
+    if sys.stdout.isatty():
+        tqdm.write(
+            "val dl {} batch {} succ {} shape {}".format(
+                dl_idx, idx, success.item(), x.shape
+            )
+        )
+
+    harness.log("vsucc", success, prog_bar=True)
+
+
 class ImitationLearningHarness(pl.LightningModule):
     """A Lightning harness for imitation learning.
 
     Override the forward() method"""
 
-    def __init__(self, lr=10e-4, entropy_bonus=10e-3):
+    def __init__(self,
+                 lr=10e-4,
+                 entropy_bonus=10e-3,
+                 optimizer_config_func=None,
+                 training_step_func=None,
+                 validation_step_func=None,
+                 ):
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters("lr", "entropy_bonus")
+        self.optimizer_config_func = optimizer_config_func or imitation_optimizer_config
+        self.training_step_func = training_step_func or imitation_training_step
+        self.validation_step_func = validation_step_func or imitation_episodic_success_validation_step
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), self.hparams.lr)
-        scheduler = torch.optim.lr_scheduler.StepLR(
-            optimizer, step_size=1000, gamma=0.9
-        )
-
-        return {"optimizer": optimizer, "scheduler": scheduler}
+        return self.optimizer_config_func(self, self.hparams.lr)
 
     def training_step(self, x, idx):
-        seeds, mission, images_path, directions_path, taken_actions, returns, masks = x
-        bool_masks = masks.bool()
+        return self.training_step_func(self, x, idx)
 
-        past_actions = (taken_actions * (taken_actions != -1))[:, :-1]
-        policy_logits, critic_values = self.forward(
-            (mission, images_path, directions_path, past_actions)
-        )
+    def validation_step(self, x, idx, dl_idx=0):
+        return self.validation_step_func(self, x, idx, dl_idx)
 
-        loss = compute_imitation_loss(
-            policy_logits[bool_masks], taken_actions[bool_masks]
-        )
-        entropy = (
-            torch.special.entr(torch.softmax(policy_logits, dim=-1).clamp(10e-7, 1.0))
-            .sum(dim=-1)
-            .mean()
-        )
-
-        self.log("tloss", loss, prog_bar=True)
-        self.log("tentropy", entropy, prog_bar=True)
-        return loss - self.hparams.entropy_bonus * entropy
-
-    def validation_step(self, x, idx, dl_idx):
-        rewards = x
-        success = (rewards > 0).to(torch.float).mean()
-
-        if sys.stdout.isatty():
-            tqdm.write(
-                "val dl {} batch {} succ {} shape {}".format(
-                    dl_idx, idx, success.item(), x.shape
-                )
-            )
-
-        self.log("vsucc", success, prog_bar=True)
-
-    def test_step(self, x, idx):
-        rewards = x
-        success = (rewards > 0).to(torch.float).mean()
-
-        self.log("tsucc", success, prog_bar=True)
+    def test_step(self, x, idx, dl_idx=0):
+        return self.validation_step_func(self, x, idx, dl_idx)
