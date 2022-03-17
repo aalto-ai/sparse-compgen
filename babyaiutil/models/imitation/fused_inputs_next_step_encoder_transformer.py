3a588c79be1447feab9995d79af787a9884dceb8
Sam Spilsbury <smspillaz@gmail.com>
2022-03-17 18:46:22 +0200

    imitation/fused_inputs_next_step_encoder_transformer: Don't pass past_actions
    (its invalid)

--- babyaiutil/models/imitation/fused_inputs_next_step_encoder_transformer.py
+++ babyaiutil/models/imitation/fused_inputs_next_step_encoder_transformer.py
@@ -57,7 +57,7 @@ class FusedInputsNextStepTransformerEncoderHarness(ImitationLearningHarness):
         mission, images_path, directions_path, past_actions = x
         batch_size, seq_len = images_path.shape[:2]
         encoded_mission, encoded_images, encoded_directions = self.sequence_encoders(
-            mission, images_path, directions_path, past_actions
+            mission, images_path, directions_path
         )
         # Flatten HW and BS dimensions
         encoded_images = encoded_images.flatten(-3, -2).flatten(0, 1)
