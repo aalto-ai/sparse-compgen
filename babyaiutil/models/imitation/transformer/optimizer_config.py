e2ef8857d22af1c563229faa4c6a4b4c8ed43d18
Sam Spilsbury <smspillaz@gmail.com>
2022-05-08 10:17:57 +0300

    transformer/optimier_config: Add warmup_proportion, decay_power, weight_decay options

--- babyaiutil/models/imitation/transformer/optimizer_config.py
+++ babyaiutil/models/imitation/transformer/optimizer_config.py
@@ -3,13 +3,18 @@ import torch.optim as optim
 from .schedule import linear_with_warmup_schedule
 
 
-def transformer_optimizer_config(harness, lr):
-    optimizer = optim.Adam(harness.parameters(), lr=lr)
+def transformer_optimizer_config(
+    harness, lr, warmup_proportion=0.14, decay_power=-2, weight_decay=0
+):
+    optimizer = optim.AdamW(harness.parameters(), lr=lr, weight_decay=weight_decay)
     return {
         "optimizer": optimizer,
         "lr_scheduler": {
             "scheduler": linear_with_warmup_schedule(
-                optimizer, 10000, harness.trainer.max_steps, -2
+                optimizer,
+                harness.trainer.max_steps * warmup_proportion,
+                harness.trainer.max_steps,
+                -2,
             ),
             "interval": "step",
             "frequency": 1,
