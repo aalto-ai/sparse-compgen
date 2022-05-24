30e2ffe271086f7af3ad582ee5946a7b6b33e6c5
Sam Spilsbury <smspillaz@gmail.com>
2022-05-24 21:30:35 +0300

    generation/generate_examples_bot: Allow support for other envs

--- scripts/generation/babyai/generate_examples_bot.py
+++ scripts/generation/babyai/generate_examples_bot.py
@@ -15,17 +15,32 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--n-trajectories-per-goal", default=10000, type=int)
     parser.add_argument("--min-trajectory-length", default=7, type=int)
+    parser.add_argument("--seeds-and-solutions-buffer", default=None)
     parser.add_argument(
-        "--seeds-and-solutions-buffer", default="seeds_and_solutions.pb"
+        "--env",
+        type=str,
+        help="Name of environment to use",
+        default="BabyAI-GoToLocal-v0",
     )
+    parser.add_argument(
+        "--n-goals",
+        type=int,
+        help="Number of unique goals in this environment",
+        default=6 * 3 * 2,
+    )
+    parser.add_argument("--n-procs", type=int, default=None)
     parser.add_argument("output", help="Where to store the resulting bot trajectories")
     args = parser.parse_args()
+
+    seeds_and_solutions_buffer = (
+        args.seeds_and_solutions_buffer or f"seeds_and_solutions_{args.env}.pb"
+    )
 
     (
         seeds_and_solutions,
         unique_environments_counter,
     ) = generate_seeds_and_action_trajectories(
-        "BabyAI-GoToLocal-v0",
+        args.env,
         args.n_trajectories_per_goal,
         6 * 3 * 2,
         args.min_trajectory_length,
@@ -45,7 +60,7 @@ def main():
     all_data = list(
         tqdm(
             replay_actions_for_data_mp(
-                "BabyAI-GoToLocal-v0", word2idx, seeds_and_solutions
+                args.env, word2idx, seeds_and_solutions, num_procs=args.n_procs
             ),
             total=len(seeds_and_solutions),
         )
